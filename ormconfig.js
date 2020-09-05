1ecf98b79f544390e5d4d02fadbdaf3033e1f396
Bruno Ferraz Silveira <bfssfb@outlook.com.br>
2020-09-05 11:53:52 -0300

    :construction: Docker configuration

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,4 @@
+/*
 console.log('process.env.DATABASE_URL :>> ', process.env.DATABASE_URL);
 console.log('process.env.DATABASE_URL :>> ', process.env.ENTITIES);
 console.log('process.env.DATABASE_URL :>> ', process.env.MIGRATIONS);
@@ -25,4 +26,16 @@ module.exports = {
   ],
   "entitiesDir": "src/models/"
   }
+}
+*/
+
+module.exports = {
+  "type": "postgres",
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
+  "entities": ["dist/**/*.entity{.ts,.js}"],
+  "synchronize": process.env.DB_SYNC == "true"
 }
